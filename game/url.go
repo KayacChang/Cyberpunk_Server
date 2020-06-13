b6fcca519892146df4e16fcfefdbcd26806e6b9b
sony79410 <sony79410@gmail.com>
2020-06-14 00:30:29 +0800

    fix db insert error order not end

--- game/url.go
+++ game/url.go
@@ -164,9 +164,23 @@ func (g *Game) gameresult(w http.ResponseWriter, r *http.Request, ps httprouter.
 		msg,
 	)
 	if errMsg.ErrorCode != code.OK {
-		g.Server.HTTPResponse(w, resultMap, errMsg)
+		fmt.Println(resultMap, errMsg)
+	}
+
+	_, errproto, err = g.EndOrder(proto.Token, order)
+	if errproto != nil {
+		errMsg := messagehandle.New()
+		errMsg.Msg = fmt.Sprintf("%d : %s:", errproto.GetCode(), errproto.GetMessage())
+		errMsg.ErrorCode = code.NewOrderError
+		g.Server.HTTPResponse(w, "", errMsg)
 		return
 	}
-	g.EndOrder(proto.Token, order)
+	if err != nil {
+		errMsg := messagehandle.New()
+		errMsg.Msg = err.Error()
+		errMsg.ErrorCode = code.NewOrderError
+		g.Server.HTTPResponse(w, "", errMsg)
+		return
+	}
 	g.Server.HTTPResponse(w, resultMap, messagehandle.New())
 }
--- go.mod
+++ go.mod
@@ -8,7 +8,4 @@ require (
 	github.com/gomodule/redigo v2.0.0+incompatible
 	github.com/gorilla/websocket v1.4.2
 	github.com/joho/godotenv v1.3.0
-	golang.org/x/lint v0.0.0-20190313153728-d0100b6bd8b3 // indirect
-	golang.org/x/tools v0.0.0-20190524140312-2c0ae7006135 // indirect
-	honnef.co/go/tools v0.0.0-20190523083050-ea95bdfd59fc // indirect
 )
